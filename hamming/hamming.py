a5af3dbbfb648667ae39f1dbb0e2498ebe3c5762
katridi <45664872+katridi@users.noreply.github.com>
2019-04-19 14:16:09 +0300

    Update hamming.py

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,11 +1,7 @@
 def distance(strand_a, strand_b): 
-    s = 0
-    check_lenght(strand_a, strand_b)
-    for l1,l2 in zip(strand_a, strand_b):
-        if l2 != l1:
-            s += 1
-    return s
+    check_length(strand_a, strand_b)
+    return sum([l2 != l1 for l1,l2 in zip(strand_a, strand_b)])
 
-def check_lenght(strand_a, strand_b):
+def check_length(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError('Sequences must have equal lenght')
